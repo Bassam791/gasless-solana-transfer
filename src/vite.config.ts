b287d917a637c218328f691647e3734bc4230415
Bassam Qahtan <81881080+Bassam791@users.noreply.github.com>
2025-06-15 02:18:39 +0300

    Update vite.config.ts

--- src/vite.config.ts
+++ src/vite.config.ts
@@ -3,4 +3,10 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    include: [
+      '@solana/wallet-adapter-react',
+      '@solana/wallet-adapter-react-ui'
+    ]
+  }
 });
